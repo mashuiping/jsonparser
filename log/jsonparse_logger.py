cedec5f67ca3c522b80e725e46b499eb4709a5a0
YAJUENET\gzs10400 <1647934940@qq.com>
2017-06-23 17:16:12 +0800

    修复bug：JsonParseLogger类中的几个函数去除self参数

--- log/jsonparse_logger.py
+++ log/jsonparse_logger.py
@@ -11,21 +11,21 @@ class JsonParseLogger(object):
                             filemode='w')
 
     @staticmethod
-    def debug(self, message_to_log):
+    def debug( message_to_log):
         logging.debug(message_to_log)
 
     @staticmethod
-    def info(self, message_to_log):
+    def info(message_to_log):
         logging.debug(message_to_log)
 
     @staticmethod
-    def warning(self,message_to_log):
+    def warning(message_to_log):
         logging.warning(message_to_log)
 
     @staticmethod
-    def error(self, message_to_log):
+    def error(message_to_log):
         logging.error(message_to_log)
 
     @staticmethod
-    def critical(self, message_to_log):
+    def critical(message_to_log):
         logging.critical(message_to_log)
--- tests/jsonparser_unittest.py
+++ tests/jsonparser_unittest.py
@@ -1,27 +1,53 @@
+# coding: utf-8
 import unittest
 from JsonParser import jsonparser
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.test_function = jsonparser.JsonParser.loads
+        self.test_function = jsonparser.JsonParser().loads
 
     def test_parse_true(self):
         # 测试引发异常
-        self.assertRaises(ValueError, self.test_function(u"\r\rTrue\r\r"))
-        self.assertRaises(ValueError, self.test_function(u"\n\ntRue\n\n"))
-        self.assertRaises(ValueError, self.test_function(u"\t\n\r truE\n\t\r "))
-        self.assertRaises(ValueError, self.test_function(u"\t\n\r tRUE\n\t\r "))
-        # 通过日志查看
+        with self.assertRaises(ValueError):
+            self.test_function(u"\r\rTrue\r\r")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\n\ntRue\n\n")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\t\n\r truE\n\t\r ")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\t\n\r tRUE\n\t\r ")
+        # 没有错误没有异常抛出，通过日志查看
         self.test_function(u"\t\ntrue")
         self.test_function(u"\ttrue\n")
 
     def test_parse_false(self):
-        pass
+        # 测试引发异常
+        with self.assertRaises(ValueError):
+            self.test_function(u"\r\rFalse\r\r")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\n\nfaLSe\n\n")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\t\n\r fAlse\n\t\r ")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\t\n\r FALSE\n\t\r ")
+        # 没有错误没有异常抛出，通过日志查看
+        self.test_function(u"\t\nfalse")
+        self.test_function(u"\tfalse\n")
 
     def test_parse_null(self):
-        pass
-
+        # 测试引发异常
+        with self.assertRaises(ValueError):
+            self.test_function(u"\r\rnuLl\r\r")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\n\ntNUll\n\n")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\t\n\r NUll\n\t\r ")
+        with self.assertRaises(ValueError):
+            self.test_function(u"\t\n\r nulL\n\t\r ")
+        # 没有错误没有异常抛出，通过日志查看
+        self.test_function(u"\t\nnull")
+        self.test_function(u"\tnull\n")
 
 if __name__ == '__main__':
     unittest.main()
