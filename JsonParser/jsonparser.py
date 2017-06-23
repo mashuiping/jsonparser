0175b94fc9d2c1f6724b4e8ec910554ad044f59d
YAJUENET\gzs10400 <1647934940@qq.com>
2017-06-23 15:17:27 +0800

    添加对true、false、null等3个字面量的解析

--- JsonParser/jsonparser.py
+++ JsonParser/jsonparser.py
@@ -12,37 +12,108 @@ class JsonParser(object):
     遵循JSON格式定义确保相同的同构数据源彼此转换后数据仍然一致
 
     支持将数据以JSON格式存储到文件并加载回来使用
+
     """
 
     def __init__(self):
-        """
-        添加函数
-        """
-        self._data = {}
+        self.data = {}
+        self.PARSE_OK = 0
+        self.PARSE_INVALID_VALUE = 1
 
-    def loads(self, s):
+    def __json_parse_true(self, json_string):
+        if json_string != 'true':
+            return self.PARSE_INVALID_VALUE, json_string
+        else:
+            json_string = json_string[4:]
+            return self.PARSE_OK, json_string
+
+    def __json_parse_false(self, json_string):
+        if json_string != 'false':
+            return self.PARSE_INVALID_VALUE, json_string
+        else:
+            json_string = json_string[5:]
+            return self.PARSE_OK, json_string
+
+    def __json_parse_null(self, json_string):
+        if json_string != 'null':
+            return self.PARSE_INVALID_VALUE, json_string
+        else:
+            json_string = json_string[4:]
+            return self.PARSE_OK, json_string
+
+    def loads(self, json_string):
+        """
+        :param s: JSON格式数据，S为一个JSON字符串
+        :return: 无返回值
+        若遇到JSON格式错误的应该抛异常，JSON中数据如果超过
+        Python里的浮点数上限的，也抛出异常。JSON的最外层假定为Object
+        """
         pass
 
     def dumps(self):
+        """
+        :return: JSON格式内容
+        将实例中的内容转成JSON格式返回
+        """
         pass
 
     def load_file(self, f):
+        """
+        :param f: 待读取的文本路径
+        :return: 无返回值
+        从文件中读取JSON格式数据
+        文件操作失败抛出异常，异常处理和loads函数相同
+        """
         pass
 
     def dump_file(self, f):
+        """
+        :param f:待保存的文件路径
+        :return: 无返回值
+        将实例中的内容以JSON格式存入文件，
+        文件若存在则覆盖，
+        文件操作失败抛出异常
+        """
         pass
 
     def load_dict(self, d):
+        """
+        :param d: Python字典数据
+        :return: 无返回值
+        将dict中读取数据存入实例中，若遇到不是字符串的key则忽略
+        """
+
         pass
 
     def dump_dict(self):
+        """
+        :return: 字典，包含实例中的内容
+        返回一个字典，包含实例中的内容
+        """
         pass
 
     def __getitem__(self, item):
+        """
+        :param item: 键值item
+        :return: 键值为item的value值
+        获取self._data[iter]
+        """
+
         pass
 
     def __setitem__(self, key, value):
+        """
+        :param key: 键值key
+        :param value: 键值key对应的value值
+        :return: 无返回值
+        给字典添加键值为key的值value
+        """
         pass
 
     def update(self,d):
+        """
+        :param d: 新加入字典数据
+        :return: 无返回值
+        用字典d更新实例中的数据，类似于字典的update
+        """
         pass
