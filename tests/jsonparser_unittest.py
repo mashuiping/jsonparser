f7e6487ce753a37cf927b3226b63de09bd1d6d9a
YAJUENET\gzs10400 <1647934940@qq.com>
2017-06-23 16:45:31 +0800

    编写测试用例

--- tests/jsonparser_unittest.py
+++ tests/jsonparser_unittest.py
@@ -4,25 +4,19 @@ from JsonParser import jsonparser
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-
         self.test_function = jsonparser.JsonParser.loads
 
-    def test_parse_space(self):
-
-
-
-    def test_invalid_space(self):
-        pass
-
     def test_parse_true(self):
-
-    def test_invalid_true(self):
-        pass
+        # 测试引发异常
+        self.assertRaises(ValueError, self.test_function(u"\r\rTrue\r\r"))
+        self.assertRaises(ValueError, self.test_function(u"\n\ntRue\n\n"))
+        self.assertRaises(ValueError, self.test_function(u"\t\n\r truE\n\t\r "))
+        self.assertRaises(ValueError, self.test_function(u"\t\n\r tRUE\n\t\r "))
+        # 通过日志查看
+        self.test_function(u"\t\ntrue")
+        self.test_function(u"\ttrue\n")
 
     def test_parse_false(self):
-        pass
-
-    def test_invalid_false(self):
         pass
 
     def test_parse_null(self):
