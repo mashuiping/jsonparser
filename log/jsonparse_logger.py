ba3eb0b6e0601c5fc00bcea796cf26dbd37c2fbb
YAJUENET\gzs10400 <1647934940@qq.com>
2017-06-23 15:58:17 +0800

    修改日志类,完成不同级别提示功能

--- log/jsonparse_logger.py
+++ log/jsonparse_logger.py
@@ -3,14 +3,29 @@ import logging
 
 
 class JsonParseLogger(object):
-    def __init__(self, message_to_log):
+    def __init__(self):
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                             datefmt='%a, %d %b %Y %H:%M:%S',
                             filename='jsonparser.log',
                             filemode='w')
+
+    @staticmethod
+    def debug(self, message_to_log):
         logging.debug(message_to_log)
-        logging.info(message_to_log)
+
+    @staticmethod
+    def info(self, message_to_log):
+        logging.debug(message_to_log)
+
+    @staticmethod
+    def warning(self,message_to_log):
         logging.warning(message_to_log)
+
+    @staticmethod
+    def error(self, message_to_log):
         logging.error(message_to_log)
+
+    @staticmethod
+    def critical(self, message_to_log):
         logging.critical(message_to_log)
